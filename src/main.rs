52d6d5b61930691fa261638bb60e60513541c711
Stribunchick <seregailin08@gmail.com>
2026-09-17 22:13:15 +0300

    All tests are passed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
+use std::io::Read;
+
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("read error");
+    io::stdin().read_to_string(&mut input).expect("read error");
     let numbers: Vec<i64> = input
         .split_whitespace()
         .map(|s| s.parse().expect("parse error"))
