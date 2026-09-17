ca77874f07a50ea5bcc83b8bf23f7662977f67a7
Stribunchick <seregailin08@gmail.com>
2026-09-17 21:28:41 +0300

    Complete homework

---
+++ main.rs
@@ -0,0 +1,8 @@
+use std::io;
+fn main(){
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).ok().expect("read error");
+    let numbers: Vec<i64> = input.split_whitespace().map(|s| s.parse().expect("parse error")).collect();
+    let sum: i64 = numbers.into_iter().sum();
+    println!("{}", sum);
+}
