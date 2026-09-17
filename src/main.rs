c874a719e410c2e90038fd0e9a9f971a694e3e36
Stribunchick <seregailin08@gmail.com>
2026-09-17 21:40:46 +0300

    Fixed formatting according to cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::io;
-fn main(){
+fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).ok().expect("read error");
-    let numbers: Vec<i64> = input.split_whitespace().map(|s| s.parse().expect("parse error")).collect();
+    io::stdin().read_line(&mut input).expect("read error");
+    let numbers: Vec<i64> = input
+        .split_whitespace()
+        .map(|s| s.parse().expect("parse error"))
+        .collect();
     let sum: i64 = numbers.into_iter().sum();
     println!("{}", sum);
 }
